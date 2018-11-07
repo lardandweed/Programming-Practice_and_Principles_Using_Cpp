14c499ad519ed2c9c4625ea0ffb2c241e08123c4
lardandweed <lardandweed@gmail.com>
2018-11-07 14:39:26 +0800

    Add files via upload

---
+++ Chapter 4/Chapter 4 Exercises/Ex_04.06.cpp
@@ -0,0 +1,39 @@
+// convert digits to spelled out numbers and vice versa
+#include "../../../std_lib_facilities.h"
+
+void convert(string strToConvert, string numSpellForm, string numDigitForm);
+
+int main()
+{
+	vector<string> spelledNum = { "zero", "one", "two", "three", "four", "five",
+								"six", "seven", "eight", "nine" };
+	vector<string> digit = { "0", "1", "2", "3", "4", "5",
+							"6", "7", "8", "9" };
+	string input = "?";
+	bool okInput = false;
+
+	// check validity of user input
+	while (okInput == false) {
+		cout << "Enter a digit (in spelled or numerical form):\n";
+		cin >> input;
+		for (int i = 0; i < 10; ++i)
+			if (input == spelledNum[i] || input == digit[i]) {
+				// input is valid >>> convert and print
+				convert(input, spelledNum[i], digit[i]);
+				okInput = true;
+				break;
+			}
+		if (okInput == true)
+			break;
+		else
+			cout << "That is not a valid input. ";
+	}
+}
+
+void convert(string input, string spellForm, string digitForm)
+{
+	if (input == spellForm)
+		cout << input << " in digit form is " << digitForm << ".\n";
+	else
+		cout << input << " spelled out is " << spellForm << ".\n";
+}
